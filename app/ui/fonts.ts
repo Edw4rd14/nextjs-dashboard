83a0abb52eafd39810527a036503ec10093553ec
Edw4rd14 <edw4rd20005@gmail.com>
2024-08-26 14:36:06 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Lusitana } from "next/font/google";
+
+export const poppins = Poppins({style:['normal'], weight:['300'], subsets:['latin']});
+export const lusitana = Lusitana({subsets:['latin'], weight:['400']});
